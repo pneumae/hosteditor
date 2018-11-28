aeeb127178be614432fe459b0a128cfb9ba1d8d7
Hong <hong226@gmail.com>
2018-11-28 22:33:58 +0100

    Improve user input fields and fixed program not terminating

--- hosteditor.py
+++ hosteditor.py
@@ -1,48 +1,65 @@
 import platform
-from shutil import copyfile
+from shutil import copy
 #from tkinter import Tk
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 
-print(platform.system())
+#print(platform.system())
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+root = Tk()
+root.withdraw()
+filename = askopenfilename()
+root.destroy()
 #print(filename)
 
-copyfile(filename,'./test')
+copy(filename,'./temphost')
 
 #f = open(filename, 'r')
 #message = f.read()
 #print(message)
 #f.close()
 
+ip = ""
+hostname = ""
 
-#f = open('./test', 'r')
-ip = "127.0.0.1"
-hostname = "home"
-
-f = open('./test', 'a')
+f = open('./temphost', 'a')
 #message = f.read()
 #print(message)
+
+
+widgettext = ""
+
+def drawInputWindow(inputField):
+
+    def printtext():
+        global e
+        global widgettext
+        string = e.get()
+        widgettext = string
+        root.destroy()
+
+    root = Tk()
+
+    root.title('Name')
+
+    t = Label(root, text=inputField)
+    t.pack()
+
+    global e
+    e = Entry(root)
+    e.pack()
+    e.focus_set()
+
+    b = Button(root,text='Submit',command=printtext)
+    b.pack(side='bottom')
+#root.destroy()
+    root.mainloop()
+
+drawInputWindow("IP")
+ip = widgettext
+drawInputWindow("Hostname")
+hostname = widgettext
+
 f.write('\n' + ip + '\t' + hostname)
 
 f.close()
-
-def printtext():
-        global e
-        string = e.get()
-        print(string)
-
-
-root = Tk()
-
-root.title('Name')
-
-e = Entry(root)
-e.pack()
-e.focus_set()
-
-b = Button(root,text='okay',command=printtext)
-b.pack(side='bottom')
-root.mainloop()
